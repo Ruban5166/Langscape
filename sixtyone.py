d65b0f9c9e881693a5eeccbe3d73ce957e8b89d1
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2021-12-05 23:25:43 +0530

    Add files via upload

---
+++ sixtyone.py
@@ -0,0 +1,18 @@
+#1 1 1 1 1 
+#1 1 1 1 
+#1 1 1
+#1 1
+#1
+
+x = 5
+while x>0:
+    num = 1
+    col = 1
+    while col<=x: 
+        print(num,end ='')
+        col= col+1
+        
+        
+    print()   
+    
+    x=x-1
