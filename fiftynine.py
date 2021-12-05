857030130a988641e2869f059b69307a4f785642
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2021-12-05 22:25:40 +0530

    Add files via upload

---
+++ fiftynine.py
@@ -0,0 +1,12 @@
+
+
+row = 1
+while row<=5:#1 
+    col = 1
+    while col<=5:#1 
+        print(col,end = ' ' )
+       
+        col= col+row #3
+    print()
+    row = row+1
+    col = 1
---
+++ sixty.py
@@ -0,0 +1,34 @@
+#triangle
+col= 1
+row= 1
+i = 1
+count = 1
+while count<= 5:#1 2 3
+    while col<=row: # r =3 c= 2 
+        print(i , end =' ')  # 1 2 3
+        i =i+1 
+        col=col+1
+    print()    
+    i = 1
+    count =count+1
+    row =row+1 
+    col = 1
+
+
+
+
+#triangle
+col= 1
+row= 1
+i = 1
+count = 1
+while count<= 5:#1 2 3
+    while col<=row: # r =3 c= 2 
+        print(i , end =' ')  # 1 2 3
+        i =i+1 
+        col=col+1
+    print()    
+    i = 1
+    count =count+1
+    row =row*2 
+    col = 1
