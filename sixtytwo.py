61a467b454be1426735bbcb76aab5e681db0b849
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2021-12-05 23:47:09 +0530

    Add files via upload

---
+++ sixtytwo.py
@@ -0,0 +1,9 @@
+row=5
+while row>0:#5 4 3 2 1
+    col = 1 #1
+    while col<=row: #col = 1 row = 1
+        print(1,end =' ') #1 1 1 1 1
+        col = col+1       #1 1 1 1  
+    print()               #1 1 1  
+    row=row-1             #1 1 
+                          #1  
